d854848dd066f40e9e32beb6cd291cd9a1ce2103
vinicodex <vinicodex@gmail.com>
2025-01-02 18:57:45 -0300

    feat: setup localstorage to reade movies

--- src/pages/Favorites/index.js
+++ src/pages/Favorites/index.js
@@ -1,9 +1,27 @@
+import { useEffect, useState } from "react";
 import "./favorites.css";
 
 function Favorites() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const myList = localStorage.getItem("@movieflix");
+    setMovies(JSON.parse(myList) || []);
+    console.log(myList);
+  }, []);
+
   return (
     <div>
       <h1>Favorite movies</h1>
+      <ul>
+        {movies.map((item) => {
+          return (
+            <li key={item.id}>
+              <span>{item.title}</span>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
