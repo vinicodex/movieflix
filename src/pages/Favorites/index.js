66176a7a250f15c75c318a110c4b5de2a4bcc10c
vinicodex <vinicodex@gmail.com>
2025-01-02 18:50:01 -0300

    feat: add /favorites route to render Favorite component

---
+++ src/pages/Favorites/index.js
@@ -0,0 +1,11 @@
+import "./favorites.css";
+
+function Favorites() {
+  return (
+    <div>
+      <h1>Favorite movies</h1>
+    </div>
+  );
+}
+
+export default Favorites;
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import Movie from "./pages/Movies";
 import Header from "./components/Header";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Erro from "./pages/Errors/notFound";
+import Favorites from "./pages/Favorites";
 
 function RoutesApp() {
   return (
@@ -11,6 +12,8 @@ function RoutesApp() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movie/:id" element={<Movie />} />
+        <Route path="/favorites" element={<Favorites />} />
+
         <Route path="*" element={<Erro />} />
       </Routes>
     </BrowserRouter>
